3b0593b8c6ad752b5f33222f478710779296ecca
Jessica <jessicachng@gmail.com>
2018-10-14 14:06:09 +0800

    completed assignment

--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -19,16 +19,52 @@ class SaberCrystal(object):
 
     def __add__(self,other):
         if isinstance(other, tuple):
-            other=SaberCrystal(color=other)
+            other = SaberCrystal(color=other)
         
-        new_color = [self.color[0] + other.color[0], self.color[1] + other.color[1], self.color[2] + other.color[2]]
-        newlist=[]
-        for value in new_color: 
-            if value> 255:
+        rgb_values = [self.red + other.red, self.green + other.green, self.blue + other.blue]
+        '''list comprehension for following expression:
+        new_color = []
+        for value in rgb_values: 
+            if value > 255:
                 value = 255
-            newlist.append(value)        
+            new_color.append(value)'''        
+        new_color = [255 if value > 255 else value for value in rgb_values]
 
-        return SaberCrystal(tuple(newlist))
+        return SaberCrystal(new_color)
 
+    def __iadd__(self,other):
+        if isinstance(other, tuple):
+            other = SaberCrystal(color=other)
+        self.red = self.red + other.red
+        self.green = self.green + other.green
+        self.blue = self.blue + other.blue
+        rgb_values = [self.red, self.green, self.blue]
+        new_color = [255 if value > 255 else value for value in rgb_values]        
+        return SaberCrystal(new_color)
 
+    def __sub__(self,other):
+        if isinstance(other, tuple):
+            other = SaberCrystal(color=other)
+        
+        rgb_values = [self.red - other.red, self.green - other.green, self.blue - other.blue]
+        new_color = [0 if value < 0 else value for value in rgb_values]       
 
+        return SaberCrystal(new_color)
+
+    def __isub__(self,other):
+        if isinstance(other, tuple):
+            other = SaberCrystal(color=other)
+        self.red = self.red - other.red
+        self.green = self.green - other.green
+        self.blue = self.blue - other.blue
+        rgb_values = [self.red, self.green, self.blue]
+        new_color = [0 if value <=0 else value for value in rgb_values]     
+        return SaberCrystal(new_color)
+
+    def __contains__(self, other):
+        if isinstance(other, tuple):
+            other = SaberCrystal(color=other)
+        if other.red <= self.red and other.blue <= self.blue and other.green <= self.green:
+            return True
+        else:
+            return False
